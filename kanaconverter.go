7d5f6cb355ecb858c1b233645fb259706bf649e2
gevgev <gevgev@yahoo.com>
2016-10-06 17:29:44 -0700

    added pipeline, still does not work

--- kanaconverter.go
+++ kanaconverter.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os/exec"
 )
@@ -9,10 +10,26 @@ import (
 func main() {
 	inputString := "inputFile.txt"
 
-	out, err := exec.Command("kakasi", "-JK", "-HK", "-s", "-o utf8", "-i utf8", inputString).CombinedOutput()
+	cmd := exec.Command("kakasi", "-JK", "-HK", "-s", "-o utf8", "-i utf8", inputString)
+	stdout, err := cmd.StdoutPipe()
+
 	if err != nil {
 		log.Fatal(err)
 	}
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	b, err := ioutil.ReadAll(stdout)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("Input: ", inputString)
-	fmt.Printf("Katakana: %s\n", out)
+	fmt.Printf("Katakana: %s\n", b)
 }
