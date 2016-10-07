b8d41f0d2310860205e94cd828a075a6fda66cfd
gevgev <gevgev@yahoo.com>
2016-10-06 18:09:37 -0700

    stdin/stdout pipeline works now

--- kanaconverter.go
+++ kanaconverter.go
@@ -1,35 +1,53 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"bytes"
+	//"fmt"
+	"io"
 	"log"
+	"os"
 	"os/exec"
 )
 
 func main() {
-	inputString := "inputFile.txt"
+	inputText := "私の名前はキコです\n"
 
-	cmd := exec.Command("kakasi", "-JK", "-HK", "-s", "-o utf8", "-i utf8", inputString)
+	runKanaConverter(populateStdin(inputText))
+	//fmt.Printf("Katakana: %s\n", out)
+}
+
+func runKanaConverter(populate_stdin_func func(io.WriteCloser)) {
+	args := []string{"-JK", "-HK", "-s", "-o", "utf8", "-i", "utf8"}
+
+	cmd := exec.Command("kakasi", args...)
+
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	stdout, err := cmd.StdoutPipe()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := cmd.Start(); err != nil {
-		log.Fatal(err)
-	}
-
-	b, err := ioutil.ReadAll(stdout)
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if err := cmd.Wait(); err != nil {
+	err = cmd.Start()
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Input: ", inputString)
-	fmt.Printf("Katakana: %s\n", b)
+	populate_stdin_func(stdin)
+	io.Copy(os.Stdout, stdout)
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func populateStdin(str string) func(io.WriteCloser) {
+	return func(stdin io.WriteCloser) {
+		defer stdin.Close()
+		io.Copy(stdin, bytes.NewBufferString(str))
+	}
 }
